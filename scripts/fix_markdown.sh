4d4d14ca952550fd4e316c1ec1c20a4c8f2b7cf1
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-07-06 10:15:03 +0200

    Better scripts

--- scripts/fix_markdown.sh
+++ scripts/fix_markdown.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-markdownlint "**/*.md"
+#
+# Fix the markdown style
+#
+# Usage:
+#
+#   ./scripts/fix_markdown.sh
+
+if [[ "$PWD" =~ scripts$ ]]; then
+    echo "FATAL ERROR."
+    echo "Please run the script from the project root. "
+    echo "Present working director: $PWD"
+    echo " "
+    echo "Tip: like this"
+    echo " "
+    echo "  ./scripts/fix_markdown.sh"
+    echo " "
+    exit 42
+fi
+
+markdownlint "**/*.md" --fix
