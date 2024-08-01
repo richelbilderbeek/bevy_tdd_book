3cef9ae32d1504f0b61fb1b7beec096d8cd9a5e3
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-08-01 21:00:06 +0200

    Only check code in 'chapters' folder

--- scripts/helper_functions.R
+++ scripts/helper_functions.R
@@ -9,17 +9,17 @@ get_all_chapter_filenames <- function() {
   # Remove some files that are not in chapters
   # Check that they exist
   
-  md_files <- stringr::str_subset(all_md_files, "faq.md", negate = TRUE) 
-  testthat::expect_equal(length(all_md_files)- 1, length(md_files))
+  # md_files <- stringr::str_subset(all_md_files, "faq.md", negate = TRUE) 
+  # testthat::expect_equal(length(all_md_files)- 1, length(md_files))
   
-  md_files <- stringr::str_subset(md_files, "definitions.md", negate = TRUE) 
-  testthat::expect_equal(length(all_md_files)- 2, length(md_files))
+  # md_files <- stringr::str_subset(md_files, "definitions.md", negate = TRUE) 
+  # testthat::expect_equal(length(all_md_files)- 2, length(md_files))
   
-  md_files <- stringr::str_subset(md_files, "introduction.md", negate = TRUE) 
-  testthat::expect_equal(length(all_md_files)- 3, length(md_files))
+  # md_files <- stringr::str_subset(md_files, "introduction.md", negate = TRUE) 
+  # testthat::expect_equal(length(all_md_files)- 3, length(md_files))
   
-  md_files <- stringr::str_subset(md_files, "functions.md", negate = TRUE) 
-  testthat::expect_equal(length(all_md_files)- 4, length(md_files))
+  # md_files <- stringr::str_subset(md_files, "functions.md", negate = TRUE) 
+  # testthat::expect_equal(length(all_md_files)- 4, length(md_files))
   
   md_files
 }
